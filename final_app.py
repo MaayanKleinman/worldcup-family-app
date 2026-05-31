ec3df8e34d98f48ff5ddc4205e7544779a3b92db
MaayanKleinman <maayan.r.k@gmail.com>
2026-05-31 22:00:47 +0300

    Update final_app.py

--- final_app.py
+++ final_app.py
@@ -14,8 +14,7 @@ GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/1BQ-O0iSj-mnTCtS8LUY-
 def init_connection():
     try:
         scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-        creds_dict = st.secrets["gcp_service_account"]
-        creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
+        creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")        creds = Credentials.from_service_account_info(creds_dict, scopes=scope)
         client = gspread.authorize(creds)
         return client.open_by_url(GOOGLE_SHEET_URL)
     except Exception as e:
